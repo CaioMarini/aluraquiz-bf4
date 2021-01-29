8a3f168cea803e3cd6ff841d8ca5723021ce59b4
CaioMarini <caioferrazmarini123@gmail.com>
2021-01-29 15:50:01 -0300

    Atualização pages Index _CM

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components'
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
+import Widget from '../src/components/widget'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
@@ -28,7 +27,7 @@ export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
-        <QuizLogo />
+        
         <Widget>
           <Widget.Header>
             <h1>{db.title}</h1>
